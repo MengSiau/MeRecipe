6f4ced6468522f20967d0e5c0baaafd88a6eb7a1
Meng Siau <hsia0003@student.monash.edu>
2024-06-06 20:59:16 +1000

    Added loading indicator when searching different webpages on webviewkit (shoppinglist page)

--- MeRecipe/ShoppingListComponents/WebViewViewController.swift
+++ MeRecipe/ShoppingListComponents/WebViewViewController.swift
@@ -12,6 +12,7 @@ class WebViewViewController: UIViewController, WKNavigationDelegate, WKUIDelegat
     
     let webview = WKWebView()
     let searchBar = UISearchBar()
+    let activityIndicator = UIActivityIndicatorView(style: .large)
     
     // Called just after the view controller's view's layoutSubviews method is invoked //
     override func viewDidLayoutSubviews() {
@@ -21,6 +22,8 @@ class WebViewViewController: UIViewController, WKNavigationDelegate, WKUIDelegat
         let searchBarHeight: CGFloat = 50
         searchBar.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.frame.width, height: searchBarHeight)
         webview.frame = CGRect(x: 0, y: view.safeAreaInsets.top + searchBarHeight, width: view.frame.width, height: view.frame.height - searchBarHeight - view.safeAreaInsets.top)
+        
+        activityIndicator.center = webview.center
     }
 
     override func viewDidLoad() {
@@ -35,6 +38,10 @@ class WebViewViewController: UIViewController, WKNavigationDelegate, WKUIDelegat
         webview.navigationDelegate = self
         webview.uiDelegate = self
         view.addSubview(webview)
+        
+        // Set up the activity indicator //
+        activityIndicator.hidesWhenStopped = true
+        view.addSubview(activityIndicator)
         
         // The default webpage to load. Could be an issue the user is not Australian //
         guard let url = URL(string: "https://woolworths.com.au") else {
@@ -58,13 +65,22 @@ class WebViewViewController: UIViewController, WKNavigationDelegate, WKUIDelegat
     // WKNavigationDelegate method: Triggered when the URL is invalid //
     func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
         displayMessage(title: "Error", message: "Unable to find your requested URL. Please try again")
+        activityIndicator.stopAnimating()
     }
 
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+         activityIndicator.startAnimating()
+     }
+     
+     // WKNavigationDelegate method: Called when the web view finishes loading //
+     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+         activityIndicator.stopAnimating()
+     }
+    
     // Helper func -> Ensures that user's input is actually a URL. //
     func isValidUrl(urlString: String) -> Bool {
         let pattern = "^(http|https)://" // accept http also.
         let regex = try? NSRegularExpression(pattern: pattern, options: .caseInsensitive)
         return regex?.firstMatch(in: urlString, options: [], range: NSRange(location: 0, length: urlString.utf16.count)) != nil // ensures the "https://" is at the start
     }
-
 }
